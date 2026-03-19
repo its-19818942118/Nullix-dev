2937c788878b20c1e98ca98bbb4b7756cd234315
Izumi <lordprime2020@gmail.com>
2026-03-19 18:26:28 -0400

    Added error details to hide away uneeded implementation clutter

---
+++ errika/include/error/error_details.hpp
@@ -0,0 +1,15 @@
+/* #pragma once */
+
+#ifndef ERROR_DETAILS_HPP_ERRIKA_
+    #define ERROR_DETAILS_HPP_ERRIKA_
+    
+    namespace
+        errirka::
+            error::_details
+    {
+        
+        
+        
+    } /* namespace errirka::error::_details */
+    
+#endif /* ERROR_DETAILS_HPP_ERRIKA_ */
---
+++ errika/source/error/error_details.cpp
@@ -0,0 +1,9 @@
+#include "error_details.hpp"
+
+namespace
+     errirka::error::_details
+{
+    
+    
+    
+} /* namespace errirka::error::_details */
